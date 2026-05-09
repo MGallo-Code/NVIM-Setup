e6f22d06ae4a9310960248dbc43ca1dfb860fab1
MGallo-Code <mgallo.code@gmail.com>
2026-05-09 02:19:43 -0400

    Disable system clipboard sync for yank/delete

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Don't sync vim registers with the system clipboard. Use "+y / "+p to opt in.
+vim.opt.clipboard = ""
